da99a4330341d18a86b8380b9ccf7969f74a5c3c
HinaChoudhry <61688489+HinaChoudhry@users.noreply.github.com>
2022-09-21 11:23:13 +0100

    Removed unused variable

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import Weather from "./Weather.js";
-import ReactDOM from "react-dom";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
