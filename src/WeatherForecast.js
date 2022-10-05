759aaefb631a7449f894175fa7d4f7127f6d1ba0
HinaChoudhry <61688489+HinaChoudhry@users.noreply.github.com>
2022-10-05 09:52:20 +0100

    Removed unused variables

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import WeatherIcon from "./WeatherIcon";
+
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,5 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
-import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
